c28bf418474548f7f98005ecdce56660d7b3fdb7
Jefferson Rodrigues <jefferson@inovadorsite.com.br>
2020-04-11 12:44:50 -0300

    Seeding Service

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -16,6 +16,11 @@ namespace SalesWebMvc.Data
 
         public DbSet<Department> Department { get; set; }
         public DbSet<Seller> Seller { get; set; }
-        public DbSet<SalesRecord> SalesRecords { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+
+        public static implicit operator SalesWebMvcContext(SeedingService v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
